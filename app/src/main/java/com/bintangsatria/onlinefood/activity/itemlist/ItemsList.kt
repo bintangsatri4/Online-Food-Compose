734fd727ed8eadd5404ca473ee6fb88886f7dd23
Bintang Satria Mulya Budi <bintangsatriamulya88@gmail.com>
2025-05-02 21:04:17 +0700

    fix: fix bug detail menu

--- app/src/main/java/com/bintangsatria/onlinefood/activity/itemlist/ItemsList.kt
+++ app/src/main/java/com/bintangsatria/onlinefood/activity/itemlist/ItemsList.kt
@@ -1,27 +1,30 @@
 package com.bintangsatria.onlinefood.activity.itemlist
 
+import android.widget.RatingBar
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -94,5 +97,41 @@ fun RowScope.FoodDetails(item: FoodModel) {
             overflow = TextOverflow.Ellipsis,
             modifier = Modifier.padding(top = 12.dp)
         )
+        TimingRow(item.TimeValue)
+        RatingBarRow(item.Star)
+    }
+}
+
+@Composable
+fun RatingBarRow(Star: Double) {
+    Row (
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(top = 8.dp)
+    ){
+        Image(
+            painter = painterResource(R.drawable.star),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(end = 8.dp)
+        )
+        Text(text = "$Star", style = MaterialTheme.typography.bodyMedium)
+    }
+}
+
+@Composable
+fun TimingRow(timeValue: Int) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .padding(top = 8.dp)
+    ) {
+        Image(
+            painter = painterResource(R.drawable.time),
+            contentDescription = null,
+            modifier = Modifier
+                .padding(end = 8.dp)
+        )
+        Text(text = "$timeValue min", style = MaterialTheme.typography.bodyMedium)
     }
 }
